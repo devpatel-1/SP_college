e1c5d42c4dcba23f29f6bf1eb358b6c38481969c
Dev Patel <devapatel09092006@gmail.com>
2026-08-01 22:55:28 +0530

    Create q7_book.js

---
+++ Int-1_Practice/q7_book.js
@@ -0,0 +1,19 @@
+// (2021) :- Create an object of book with elements id, title and authorName. Export that object in another file and fetch the specific book with title "7 habits of highly effective people" and print it on web page.
+
+var books = [
+
+    {
+        id: 1,
+        title: "7 habits of highly effective people",
+        authorName: "Stephen R. Covey"
+    },
+
+    {
+        id: 2,
+        title: "Rich Dad Poor Dad",
+        authorName: "Robert T. Kiyosaki"
+    }
+
+];
+
+module.exports = books;
